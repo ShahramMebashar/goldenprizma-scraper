60125180e54bd9d5fdfeeab0192fee32d6c7da74
Shahram M. Hassan <shahram.mebashar@gmail.com>
2022-05-29 15:34:22 +0300

    add environmental variables

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ import puppeteer from 'puppeteer';
 import * as cheerio from 'cheerio';
 import cors from 'cors';
 import ScrapManager from './ScrapManager.js';
+import dotenv from 'dotenv';
+
 
 const app = express();
+
+dotenv.config('./.env');
 
 app.use(cors({
     origin: '*'
@@ -16,9 +20,17 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', async (req, res) => {
-    const browser  = await puppeteer.launch({ headless: true });
+    const browser  = process.env.APP_MODE == "production" ? 
+        await puppeteer.launch({ 
+            // headless: true,
+            executablePath: process.env.CHROMIUM_PATH,
+            args: [ '--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote' ],
+     })
+     : await puppeteer.launch({ headless: true });
+
     const page = await browser.newPage();
     await page.emulate(Ipad);
+    
     const response = await page.goto(req.query.link, { waitUntil: 'load'});
     const $ = cheerio.load(await response.text());
     const data = ScrapManager(req.query.link, $);
