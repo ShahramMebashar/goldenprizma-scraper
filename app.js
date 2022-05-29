a44d48db5fafe879381bb6fe36001f7078e8698f
Shahram M. Hassan <shahram.mebashar@gmail.com>
2022-05-29 18:23:05 +0300

    add puppeteer stealth

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors({
     origin: '*'
 }));
 puppeteer.use(StealthPlugin());
-
+console.log(StealthPlugin().availableEvasions)
 // const Ipad = puppeteer.devices['iPad Pro 11 landscape'];
 
 app.use(express.json()) // for parsing application/json
@@ -25,27 +25,18 @@ app.get('/', async (req, res) => {
         await puppeteer.launch({ 
             headless: true,
             executablePath: process.env.CHROMIUM_PATH,
-            // args: [ '--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote' ],
      })
      : await puppeteer.launch({  
         headless: true,
-        // args: [ '--disable-gpu', '--no-sandbox',]
     });
 
     const page = await browser.newPage();
-    // await page.emulate(Ipad);
-    page.setExtraHTTPHeaders({
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9,en;q=0.8'
-    })
-    page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36');
 
     const response = await page.goto(req.query.link, { waitUntil: 'load'});
     // await page.screenshot({ path: './screenshot.png' });
-
+    await page.waitForTimeout(2000)
     const $ = cheerio.load(await response.text());
     const data = ScrapManager(req.query.link, $);
-    console.log(await response.json())
 
     await browser.close();
     res.send({ 'content': data });
